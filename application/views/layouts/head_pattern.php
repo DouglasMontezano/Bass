5ef1f2516f7e5cdb5f99304a3344dbebc839f9b7
Douglas Montezano <douglasmontezano@hotmail.com>
2018-11-23 22:56:40 -0200

    ajustes em andamento

--- application/views/layouts/head_pattern.php
+++ application/views/layouts/head_pattern.php
@@ -7,9 +7,11 @@
     <script type="text/javascript" src="<?php echo base_url(); ?>assets/jquery/jquery_3.2.js"></script>   
     <script type="text/javascript" src="<?php echo base_url(); ?>assets/bootstrap-4.0.0-dist/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="<?php echo base_url(); ?>assets/jquery/jquery.js"></script>
-    <link type="text/css" href="<?php echo base_url(); ?>assets/css/Reset.css"rel="stylesheet">
+     <link type="text/css" href="<?php echo base_url(); ?>assets/css/Reset.css"rel="stylesheet">
     <link type="text/css" href="<?php echo base_url(); ?>assets/css/nav-pattern.css"rel="stylesheet">
     <link type="text/css" href="<?php echo base_url(); ?>assets/bootstrap-4.0.0-dist/css/bootstrap.min.css"rel="stylesheet">
     <link type="text/css" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"rel="stylesheet">    
     <!-- <link href="http://pingendo.github.io/pingendo-bootstrap/themes/default/bootstrap.css" rel="stylesheet" type="text/css"> -->
+<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> -->
 <head>
--- application/views/vw_Clientes.php
+++ application/views/vw_Clientes.php
@@ -1,14 +1,14 @@
 
-    <body>
+   
         <nav class="section section-primary ">
           <div class="container">
                 <div class="row">
-                    <div class="col-md-12">
-                        <div class="col-md-4">
-                            <h2 contenteditable="true">Clientes</h2>
-                        </div>
-                    </div>                 
-                    <div class="col-md-8">
+                    <div class="col-md-12">                      
+                        <h2 contenteditable="true">Clientes</h2>
+                    </div>
+                </div> 
+                <div class="row">
+                    <div class="col-md-8">                                                  
                         <div class="col-md-1">
                             <a href="<?= base_url() ?>Clientes/ListClientes"><i class="fa fa-3x fa-fw text-inverse -plus fa-list-ol"><h5>Listar</h5></i></a>
                         </div>
@@ -17,16 +17,16 @@
                         </div>
                         <div class="col-md-1">
                             <a href="<?= base_url() ?>Administrativo/PainelAdministrativo"><i class="fa fa-3x fa-fw text-inverse fa-sliders"><h5>Painel</h5></i></a><br>
-                        </div>
+                        </div>         
                     </div>
                     <div class="col-md-4">
                         <form class="navbar-form pull-left" method="post"action="<?= base_url() ?>Clientes/GetForCli">
                             <input type="text" class="form-control" id="busca" name="busca"data-toggle="tooltip" data-placement="top"
-                                   title="Aqui você pode buscar pelo nome do cliente!  ">
+                                    title="Aqui você pode buscar pelo nome do cliente!  ">
                             <button border="0" type="submit" class="btn btn-default" ><strong>Buscar</strong></button>
-                        </form>
-                    </div>
+                        </form>                     
+                    </div>                  
                 </div>
-            </div>
+            </nav>
         </nav>
     </body>
