aea7c22713cac44d67797086a8c5a2594c287ce8
Douglas Montezano <douglasmontezano@hotmail.com>
2018-12-07 22:58:18 -0200

    nav-clientes

--- application/views/layouts/head_pattern.php
+++ application/views/layouts/head_pattern.php
@@ -5,11 +5,13 @@
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-110088301-1"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script type="text/javascript" src="<?php echo base_url(); ?>assets/jquery/jquery_3.2.js"></script>   
-    <script type="text/javascript" src="<?php echo base_url(); ?>assets/bootstrap-4.0.0-dist/js/bootstrap.min.js"></script>
+    <!-- <script type="text/javascript" src="<?php echo base_url(); ?>assets/bootstrap-4.0.0-dist/js/bootstrap.min.js"></script> -->
+    <script type="text/javascript" src="<?php echo base_url(); ?>assets/bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="<?php echo base_url(); ?>assets/jquery/jquery.js"></script>
-    <link type="text/css" href="<?php echo base_url(); ?>assets/css/Reset.css"rel="stylesheet">
+    <!-- <link type="text/css" href="<?php echo base_url(); ?>assets/css/Reset.css"rel="stylesheet"> -->
     <link type="text/css" href="<?php echo base_url(); ?>assets/css/nav-pattern.css"rel="stylesheet">
-    <link type="text/css" href="<?php echo base_url(); ?>assets/bootstrap-4.0.0-dist/css/bootstrap.min.css"rel="stylesheet">
+    <!-- <link type="text/css" href="<?php echo base_url(); ?>assets/bootstrap-4.0.0-dist/css/bootstrap.min.css"rel="stylesheet"> -->
+    <link type="text/css" href="<?php echo base_url(); ?>assets/bootstrap-4.1.3-dist/css/bootstrap.min.css"rel="stylesheet">
     <link type="text/css" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"rel="stylesheet">    
     <!-- <link href="http://pingendo.github.io/pingendo-bootstrap/themes/default/bootstrap.css" rel="stylesheet" type="text/css"> -->
 <head>
--- application/views/vw_Clientes.php
+++ application/views/vw_Clientes.php
@@ -1,32 +1,34 @@
 
-    <body>
-        <nav class="section section-primary ">
-          <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="col-md-4">
-                            <h2 contenteditable="true">Clientes</h2>
-                        </div>
-                    </div>
-                    <div class="col-md-8">
-                        <div class="col-md-1">
-                            <a href="<?=base_url()?>Clientes/ListClientes"><i class="fa fa-3x fa-fw text-inverse -plus fa-list-ol"><h5>Listar</h5></i></a>
-                        </div>
-                        <div class="col-md-1">
-                            <a href="<?=base_url()?>Clientes/FormCadClientes"><i class="fa fa-3x fa-fw text-inverse fa-user-plus"><h5>Cadastrar</h5></i></a>
-                        </div>
-                        <div class="col-md-1">
-                            <a href="<?=base_url()?>Administrativo/PainelAdministrativo"><i class="fa fa-3x fa-fw text-inverse fa-sliders"><h5>Painel</h5></i></a><br>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <form class="navbar-form pull-left" method="post"action="<?=base_url()?>Clientes/GetForCli">
-                            <input type="text" class="form-control" id="busca" name="busca"data-toggle="tooltip" data-placement="top"
-                                   title="Aqui você pode buscar pelo nome do cliente!  ">
-                            <button border="0" type="submit" class="btn btn-default" ><strong>Buscar</strong></button>
-                        </form>
-                    </div>
-                </div>
+        
+        <nav class="navbar navbar-expand-lg navbar-light  section-primary">
+            <div class="container">   
+            <a class="navbar-brand" href="#"><h2>Clientes</h2></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav col-md-12">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="<?= base_url() ?>Clientes/ListClientes"><i class="nav-link active fa fa-2x fa-fw text-inverse -plus fa-list-ol"><h5>Listar</h5></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= base_url() ?>Clientes/FormCadClientes"><i class="nav-link active fa fa-2x fa-fw text-inverse fa-user-plus"><h5>Cadastrar</h5></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= base_url() ?>Administrativo/PainelAdministrativo"><i class="nav-link active fa fa-2x fa-fw text-inverse fa-sliders"><h5>Painel</h5></i></a><br>
+                    </li>
+                    <li class="nav-item search">
+                        <div class="form ">     
+                            <form class="form-inline "method="post"action="<?= base_url() ?>Clientes/GetForCli" >
+                                <input class="form-control mr-sm-2  " name="busca"  title="Aqui você pode buscar pelo nome do cliente!"type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+                                <button class="btn btn-outline-light my-2 my-sm-0 " type="submit">Buscar</button>
+                            </form>
+                        </div>        
+                    </li>
+                </ul>
             </div>
         </nav>
-    </body>
+
+
+
+        
