ebb07f9a7a6a073a3eb172fa5cbf407f1aa800b6
Douglas Montezano <douglasmontezano@hotmail.com>
2018-12-12 14:01:31 -0200

    ajuste da nav padrão boot 4.1

--- application/views/vw_Clientes.php
+++ application/views/vw_Clientes.php
@@ -1,14 +1,16 @@
 
         
-        <nav class="navbar navbar-expand-lg navbar-light  section-primary">
-            <div class="container container-fluid">   
-                <a class="navbar-brand" href="#"><h2>Clientes</h2></a>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+
+            <div class=" container ">   
+
+                <a class="navbar-brand h1" href="#">Clientes</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav col-md-12">
-                        <li class="nav-item active">
+                    <ul class="navbar-nav mr-auto ">
+                        <li class="nav-item">
                             <a class="nav-link" href="<?= base_url() ?>Clientes/ListClientes"><i class="nav-link active fa fa-2x fa-fw text-inverse -plus fa-list-ol"><h5>Listar</h5></i></a>
                         </li>
                         <li class="nav-item">
@@ -17,7 +19,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="<?= base_url() ?>Administrativo/PainelAdministrativo"><i class="nav-link active fa fa-2x fa-fw text-inverse fa-sliders"><h5>Painel</h5></i></a><br>
                         </li>
-                        <li class="nav-item search">
+                    </ul>    
+                    <ul class= "navbar-nav ml-auto">    
+                        <li class="nav-item ">
                             <div class="form ">     
                                 <form class="form-inline "method="post"action="<?= base_url() ?>Clientes/GetForCli" >
                                     <input class="form-control mr-sm-2  " name="busca"  title="Aqui você pode buscar pelo nome do cliente!"type="search" placeholder="Pesquisar" aria-label="Pesquisar">
