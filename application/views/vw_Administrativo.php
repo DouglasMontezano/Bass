08a4f5e58b1e62924c93d37d935815df3c342441
Douglas <hemy426@gmail.com>
2019-08-23 19:50:43 -0300

    Ajustando menu do painel Adm.

--- application/views/vw_Administrativo.php
+++ application/views/vw_Administrativo.php
@@ -1,54 +1,51 @@
 <body onload="chartdataload()">
     <!-- <div class="container container-fluid "> -->
-    <nav class= "nav">
+    <nav class="nav">
         <div class="title-nav">
             <h3>Administrativo</h3>
         </div>
-        
         <ul>
             <li><a href="<?= base_url() ?>Clientes/PainelClientes"><i class="fa fa-3x fa-fw text-inverse fa-user  text-left ">
-                        <h5>Clientes</h5>
+                        Clientes
                     </i></a></li>
             <li><a href="<?= base_url() ?>Produtos_Servicos/PainelProdutos"><i class="fa fa-3x fa-fw text-inverse fa fa-product-hunt  text-left">
-                        <h5>Produtos</h5>
+                        Produtos
                     </i></a></li>
             <li><a href="<?= base_url() ?>produtos_Servicos/PainelServicos"><i class="fa fa-3x fa-fw text-inverse text-left fa-wrench">
-                        <h5>Serviços</h5>
+                        Serviços
                     </i></a></li>
-
             <li><a href="<?= base_url() ?>Os/PainelOs"><i class="fa fa-3x fa-fw text-inverse fa text-left fa-book">
-                        <h5>O.S</h5>
+                        O.S
                     </i></a></li>
             <li><a href="<?= base_url() ?>Funcionarios/PainelFuncionarios"><i class="fa fa-3x fa-fw text-inverse fa fa-street-view text-left">
-                        <h5>
-                            Funcionários</h5>
+                        Funcionários
                     </i></a></li>
 
             <li><a href="<?= base_url() ?>Fornecedores/PainelFornecedores"><i class="fa fa-3x fa-fw text-inverse fa fa-user-circle-o">
-                        <h5>Fornecedores</h5>
+                        Fornecedores
                     </i></a></li>
 
             <li><a href="<?= base_url() ?>Vendas/PainelVendas"><i class="fa fa-3x fa-fw text-inverse fa fa-shopping-cart text-left">
-                        <h5>Vendas</h5>
+                        Vendas
                     </i></a></li>
 
             <li><a href="<?= base_url() ?>Lancamentos/PainelLancamentos"><i class="fa fa-3x fa-fw text-inverse fa fa-leanpub text-left">
-                        <h5>Lançamentos</h5>
+                        Lançamentos
                     </i></a></li>
 
             <li><a href="<?= base_url() ?>Fluxo/PainelFluxo"><i class="fa fa-3x fa-fw text-inverse fa fa-usd text-left">
-                        <h5>Fluxo.C</h5>
+                        Fluxo.C
                     </i></a></li>
 
             <li> <a href="<?= base_url() ?>Relatorios/PainelRelatorios"><i class="fa fa-3x fa-fw text-inverse fa fa-file-text text-left">
-                        <h5>Relatórios</h5>
+                        Relatórios
                     </i></a></li>
 
             <li><a href="<?= base_url() ?>Login/Logout"><i class="fa fa-3x fa-fw text-inverse fa fa-sign-out" aria-hidden="true">
-                        <h5>Logout</h5>
+                        Logout
                     </i></a></li>
-        </ul>       
-    </nav>    
+        </ul>
+    </nav>
     <div class="row">
         <div class="col-md-12">
             <div class="col-md-12">
@@ -58,6 +55,6 @@
                 <div id="chart_div2"></div>
             </div>
         </div>
-    </div>    
+    </div>
 </body>
 </html>
