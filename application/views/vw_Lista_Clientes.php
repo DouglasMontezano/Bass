c6aa2d3e56aa32773f1d1f8d3ff9c8e2d7c967e2
Douglas Montezano <hemy426@gmail.com>
2020-04-15 15:53:11 -0300

    Ajustes de containner nas listagens

--- application/views/vw_Lista_Clientes.php
+++ application/views/vw_Lista_Clientes.php
@@ -1,50 +1,52 @@
-        <div class="container container-fluid">
-            <br>
-            <br>    
-            <div class="col-md-12">
-                <caption>Listando Clientes</caption>
-                <table class=" table table-striped table-dark table-bordered table-hover ">       
-                    <thead>
-                        <tr>
-                            <th> Código</th>
-                            <th> Nome</th>
-                            <th> Telefone</th>
-                            <th> CPF</th>
-                            <th> Endereço</th>
-                            <th> Número</th>
-                            <th> Bairro</th>
-                            <th> Cidade</th>
-                            <th> Estado</th>
-                            <th> Desativado</th>
-                            <th> </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($resultado as $obj) { ?>
-                            <tr>
-                                <td align="center"><?= $obj->id_cliente; ?></td>
-                                <td ><?= $obj->nome_cliente; ?></td>
-                                <td><?= $obj->fone_cliente; ?></td>
-                                <td><?= $obj->cpf_cliente; ?></td>
-                                <td><?= $obj->endereco_cliente; ?></td>
-                                <td align="center"><?= $obj->numero_casa_cliente; ?></td>
-                                <td><?= $obj->bairro_cliente; ?></td>
-                                <td><?= $obj->cidade_cliente; ?></td>
-                                <td align="center"><?= $obj->estado_cliente; ?></td>
-                                <td align="center"><?= $obj->desativado; ?></td>
-                                <td>
-                                    <a href="<?= base_url('Clientes/EditClientes/' . $obj->id_cliente) ?>"
-                                    class="btn btn-info btn-group"><i class="fa fa-pencil" aria-hidden="true"></i> </a>
-                                    <a href="<?= base_url('Clientes/ExcClientes/' . $obj->id_cliente) ?>"
-                                    class="btn btn-danger btn-group"
-                                    onclick="return confirm('Deseja realmente excluir o cliente');"><i class="fa fa-trash-o" aria-hidden="true"></i>
-                                    </a>
-                                </td>
-                            </tr>
-                        <?php } ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </body>
-</html>
+        <!-- <div class="container container-fluid"> -->
+          <br>
+          <br>
+          <div class="col-md-12">
+            <caption>Listando Clientes</caption>
+            <table class=" table table-striped table-dark table-bordered table-hover ">
+              <thead>
+                <tr>
+                  <th> Código</th>
+                  <th> Nome</th>
+                  <th> Telefone</th>
+                  <th> CPF</th>
+                  <th> Endereço</th>
+                  <th> Número</th>
+                  <th> Bairro</th>
+                  <th> Cidade</th>
+                  <th> Estado</th>
+                  <th> Desativado</th>
+                  <th> </th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php foreach ($resultado as $obj) { ?>
+                <tr>
+                  <td align="center"><?= $obj->id_cliente; ?></td>
+                  <td><?= $obj->nome_cliente; ?></td>
+                  <td><?= $obj->fone_cliente; ?></td>
+                  <td><?= $obj->cpf_cliente; ?></td>
+                  <td><?= $obj->endereco_cliente; ?></td>
+                  <td align="center"><?= $obj->numero_casa_cliente; ?></td>
+                  <td><?= $obj->bairro_cliente; ?></td>
+                  <td><?= $obj->cidade_cliente; ?></td>
+                  <td align="center"><?= $obj->estado_cliente; ?></td>
+                  <td align="center"><?= $obj->desativado; ?></td>
+                  <td>
+                    <a href="<?= base_url('Clientes/EditClientes/' . $obj->id_cliente) ?>"
+                      class="btn btn-info btn-group"><i class="fa fa-pencil" aria-hidden="true"></i> </a>
+                    <a href="<?= base_url('Clientes/ExcClientes/' . $obj->id_cliente) ?>"
+                      class="btn btn-danger btn-group"
+                      onclick="return confirm('Deseja realmente excluir o cliente');"><i class="fa fa-trash-o"
+                        aria-hidden="true"></i>
+                    </a>
+                  </td>
+                </tr>
+                <?php } ?>
+              </tbody>
+            </table>
+          </div>
+        <!-- </div> -->
+        </body>
+
+        </html>
--- application/views/vw_Lista_Funcionarios.php
+++ application/views/vw_Lista_Funcionarios.php
@@ -1,38 +1,41 @@
-<div class="container">
+    <!-- <div class="container"> -->
     <br>
     <br>
     <div class="col-md-12">
-    <caption>Listando Funcionários</caption>
-    <table class=" table table-striped table-dark table-bordered table-hover ">   
-            <thead>
+        <caption>Listando Funcionários</caption>
+        <table class=" table table-striped table-dark table-bordered table-hover ">
+        <thead>
             <tr>
-                <th> Código</th>
-                <th> Nome</th>
-                <th> E_Mail</th>
-                <th> Fone</th>
-                <th> Salário</th>
-                <th> PIS </th>
+            <th> Código</th>
+            <th> Nome</th>
+            <th> E_Mail</th>
+            <th> Fone</th>
+            <th> Salário</th>
+            <th> PIS </th>
             </tr>
-            </thead>
-            <tbody>
+        </thead>
+        <tbody>
             <?php foreach ($resultado as $obj) { ?>
-                <tr>
-                    <td><?= $obj->id_funcionario;      ?></td>
-                    <td><?= $obj->nome_funcionario;    ?></td>
-                    <td><?= $obj->email_funcionario;   ?></td>
-                    <td><?= $obj->fone_funcionario;    ?></td>
-                    <td><?=  number_format($obj->salario_funcionario, 2, ',', '.');   ?></td>
-                    <td><?= $obj->pis_funcionario;     ?></td>
-                    <td>
-                        <a href="<?= base_url('Funcionarios/EditFuncionarios/' . $obj->id_funcionario) ?>"class="btn btn-info btn-group"><i class="fa fa-pencil" aria-hidden="true"></i> </a>
-                        <a href="<?= base_url('Funcionarios/ExcFuncionarios/' . $obj->id_funcionario) ?>" class="btn btn-danger btn-group" onclick="return confirm('Deseja realmente excluir o funcionário? ');"><i class="fa fa-trash-o" aria-hidden="true"></i>
-                        </a>
-                    </td>
-                </tr>
+            <tr>
+            <td><?= $obj->id_funcionario;      ?></td>
+            <td><?= $obj->nome_funcionario;    ?></td>
+            <td><?= $obj->email_funcionario;   ?></td>
+            <td><?= $obj->fone_funcionario;    ?></td>
+            <td><?=  number_format($obj->salario_funcionario, 2, ',', '.');   ?></td>
+            <td><?= $obj->pis_funcionario;     ?></td>
+            <td align="center">
+                <a href="<?= base_url('Funcionarios/EditFuncionarios/' . $obj->id_funcionario) ?>"
+                class="btn btn-info btn-group"><i class="fa fa-pencil" aria-hidden="true"></i> </a>
+                <a href="<?= base_url('Funcionarios/ExcFuncionarios/' . $obj->id_funcionario) ?>"
+                class="btn btn-danger btn-group" onclick="return confirm('Deseja realmente excluir o funcionário? ');"><i
+                    class="fa fa-trash-o" aria-hidden="true"></i>
+                </a>
+            </td>
+            </tr>
             <?php } ?>
-            </tbody>
+        </tbody>
         </table>
     </div>
-</div>
-</body>
+    <!-- </div> -->
+    </body>
 </html>
